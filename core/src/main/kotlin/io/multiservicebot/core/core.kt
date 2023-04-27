9e42afed7faeb07ddc373f7e9c338f8381c750db
Senchick <senchickpro@gmail.com>
2023-04-27 16:40:24 +0300

    add ktor client

--- core/src/main/kotlin/io/multiservicebot/core/core.kt
+++ core/src/main/kotlin/io/multiservicebot/core/core.kt
@@ -4,12 +4,40 @@ import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import io.github.cdimascio.dotenv.Dotenv
 import io.github.cdimascio.dotenv.dotenv
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.logging.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
 import org.jetbrains.exposed.sql.Database
 
 suspend fun init(block: suspend Configuration.() -> Unit) = block(Configuration())
 
 class Configuration {
     val env = SafeDotenv(dotenv())
+    val client = HttpClient(CIO) {
+        val json = Json {
+            ignoreUnknownKeys = true
+            encodeDefaults = true
+        }
+
+        install(ContentNegotiation) {
+            json(json)
+        }
+
+        install(Logging) {
+            logger = Logger.SIMPLE
+            level = LogLevel.HEADERS
+        }
+
+        install(HttpTimeout) {
+            requestTimeoutMillis = HttpTimeout.INFINITE_TIMEOUT_MS
+            connectTimeoutMillis = HttpTimeout.INFINITE_TIMEOUT_MS
+            socketTimeoutMillis = HttpTimeout.INFINITE_TIMEOUT_MS
+        }
+    }
 
     init {
         Database.connect(
